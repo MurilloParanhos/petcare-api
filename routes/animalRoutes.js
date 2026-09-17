d235efac2fee5a8c4edd7ae83928bdda44e48cac
Murillo Paranhos <murillo_paranhos@hotmail.com>
2026-09-17 20:43:00 -0300

    Adiciona rotas PUT e DELETE de animais que faltavam no arquivo de rotas

--- routes/animalRoutes.js
+++ routes/animalRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { criarAnimal, listarAnimais } = require('../controllers/animalController');
+const { criarAnimal, listarAnimais, atualizarAnimais, deletarAnimais } = require('../controllers/animalController');
 const autenticar = require('../middlewares/autenticar');
 
 router.post('/animais', autenticar, criarAnimal);
 router.get('/animais', autenticar, listarAnimais);
+router.put('/animais/:id', autenticar, atualizarAnimais);
+router.delete('/animais/:id', autenticar, deletarAnimais);
 
 module.exports = router;
